49d06b5d4fb698316c48dcf3b0d7b95bb6bbe34c
richmondvan <evan.ty.zheng@gmail.com>
2020-08-01 18:19:02 -0400

    Updates

---
+++ precache-manifest.c8fea7eaf0983f4d41747ba2ccdb40c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bec8a5fbb4b7ba85d029e6eb7930635c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84c972d87dff6ff16222",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "780f048c227d729e0c34",
+    "url": "/static/js/2.4e5baff1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4e5baff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84c972d87dff6ff16222",
+    "url": "/static/js/main.23766bdd.chunk.js"
+  },
+  {
+    "revision": "9ff2834de3700440155e",
+    "url": "/static/js/runtime-main.8f3f6ed9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
