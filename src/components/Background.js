69faef5dbe5b9d652364462aa22d04858d8fe973
richmondvan <evan.ty.zheng@gmail.com>
2020-08-04 00:59:55 -0400

    fixed mobile scrolling

--- src/components/Background.js
+++ src/components/Background.js
@@ -7,7 +7,7 @@ function Background()
 {
     let bubbles = [];
 
-    for (let i = 0; i < 128; i++) {
+    for (let i = 0; i < 64; i++) {
         bubbles.push(<div 
             className="bubble" 
             style = {{
