0dba792e6fa9d43eb1902b34f6e2ff79b8aca309
richmondvan <evan.ty.zheng@gmail.com>
2020-08-06 16:38:17 -0400

    Fixed visual bugs with contacts

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -186,8 +186,8 @@ class Contacts extends React.Component {
                         rows="10"
                         className="field"
                         required
-                        placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                        ></textarea>
+                        placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.">
+                        </textarea>
                     </fieldset>
                     <fieldset className="pure-group honeypot-field">
                     <input id="honeypot" type="text" name="honeypot" />
